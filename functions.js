6dc1df41b1fcc58a0ef8ce35f37e9a8d8aea345a
pierfilippo <pierre.falconetti@long2.net>
2015-02-04 19:00:29 +0100

    Ajout de commentaires plus parlants parce que je n'y vois plus très clair.

--- functions.js
+++ functions.js
@@ -1,29 +1,43 @@
 function arcSegment() {
+    // Angles are expressed in radians.
+    // Complete revolution = 360° = 2*Math.PI radians
+    // E cardinal point = 0.0*Math.PI radians
+    // S cardinal point = 0.5*Math.PI radians
+    // W cardinal point = 1.0*Math.PI radians
+    // N cardinal point = 1.5*Math.PI radians
+    // arc(.,.,.,.,.,true) = counterclockwise
+    // arc(.,.,.,.,.,false) = clockwise
     
-    // Initialization of the canvas
+    // Standard initialization of the canvas
     var can = document.getElementById('myCanvas');
     var ctx = can.getContext('2d');
     
     // Parameters for the segments
-    var graphYearStart = 1997;
-    var graphYearEnd = 2015;
-    var graphYearSubs = graphYearEnd-graphYearStart+1;
-    var graphSize = 1.9;
-    var graphYearAngle = (graphSize*Math.PI)/graphYearSubs;
-    var graphYearAngleStart = 0.5*Math.PI;
-    var graphYearAngleEnd = graphYearAngleStart+graphSize*Math.PI;
-    var graphYearGap = 0.02;
+    // These parameters define the global aspect of the graph where the segments will be drawn
+    var graphYearStart = 1997; // Starting year of the first segment
+    var graphYearEnd = 2015; // Ending year for the last segment, must be superior to graphYearStart
+    var graphYearSubs = graphYearEnd-graphYearStart+1; // Markings between the starting year and the ending year (= number of years + 1), at least 2
+    var graphYearAngleStart = 0.5*Math.PI; // Position where we start drawing the graph (0.5*Math.PI = S cardinal point), in radians
+    var graphSize = 1.9*Math.PI; // Total length of the graph (2*Math.PI = complete revolution), in radians
+    var graphYearAngleEnd = graphYearAngleStart+graphSize; // Position where we end drawing the graph, in radians
+    var graphYearAngle = graphSize/graphYearSubs; // Angle occupied by one single year (-> distance between two markings), in radians
+    var graphYearGap = 0.02; // Angle of the gap used to show the beginning/ending of a year (-> size of each marking), in radians
+
     // Segments layout
-    var segRadialSubs = 8; // Number of radial subdivisions, strictly positive
+    // These parameters define the global aspect of each segment in the graph
     var segRadiusMax = 150; // Max radius for all segments: they'll be displayed in a zone of segRadiusMax*2 per segRadiusMax*2 pixels
-    var segRadialWidth = segRadiusMax/segRadialSubs;
+    var segRadialSubs = 8; // Number of radial subdivisions between the center and the outer border of all the graphs, strictly positive
+    var segRadialWidth = segRadiusMax/segRadialSubs; // Width (-> thickness) of a single radial subdivision, in pixels
     var segWidthProportion = 2; // Segment width, in number of radial subdivisions, stricly positive
-    var segWidth = segRadialWidth*segWidthProportion;
-    // Segments common center
-    var segCenterX = 200;
-    var segCenterY = 200;
+    var segWidth = segRadialWidth*segWidthProportion; // Width (-> thickness) of a segment, in pixels
+    var segCenterX = 200; // Common X coordinate for the center of all the segments, in pixels
+    var segCenterY = 200; // Common Y coordinate for the center of all the segments, in pixels
+
     // Segment individual properties
-    var segLevel = 6; // Starting radial level for drawing the segment
+    var segLevel = 6; // Starting radial level for drawing the segment (= <= segRadialSubs - segWidthProportion)
+
+
+
     var radius = segRadialWidth*segLevel+segWidth/2+0.5;
     var segGradientRadiusStart = segLevel*segRadialWidth; // Inner border of the segment = starting point for radial gradient
     var segGradientRadiusEnd = (segLevel+segWidthProportion)*segRadialWidth; // Outer border of the segment = ending point for radial gradient
@@ -31,7 +45,7 @@ function arcSegment() {
     segment.addColorStop(0,'#f00'); // Inner color for gradient
     segment.addColorStop(1,'#900'); // Outer color for gradient
     var segYearStart = 1997.5; /* FIXME PPF */
-    var segYearEnd = 2014;
+    var segYearEnd = 2013.5;
     var segStart = graphYearAngleStart+(segYearStart-graphYearStart)*graphYearAngle;
     var segStart2= graphYearAngleStart+(Math.ceil(segYearStart)-graphYearStart)*graphYearAngle; /* FIXME PPF */
     var segEnd = graphYearAngleStart+(segYearEnd-graphYearStart)*graphYearAngle;
