cafd2a080f864811ba6bdae66ef3be0f389128ad
pierfilippo <pierre.falconetti@long2.net>
2015-02-12 16:10:09 +0100

    Prise en charge de la dernière fraction d'année (en cas de date de fin non entière).

--- functions.js
+++ functions.js
@@ -45,9 +45,9 @@ function arcSegment() {
 
 
     var segYearStart = 1997.5; /* FIXME PPF */
-    var segYearEnd = 2013.5;
-    var segStart = graphYearAngleStart+(segYearStart-graphYearStart)*graphYearAngle;
-    var segFirstNewYear= graphYearAngleStart+(Math.ceil(segYearStart)-graphYearStart)*graphYearAngle; /* FIXME PPF */
+    var segYearEnd = 2013.5; /* FIXME PPF */
+    var segStart = graphYearAngleStart+(segYearStart-graphYearStart)*graphYearAngle; // Starting position of the segment
+    var segFirstNewYear = graphYearAngleStart+(Math.ceil(segYearStart)-graphYearStart)*graphYearAngle; // Position of the first new-year-gap encountered by the segment
     var segEnd = graphYearAngleStart+(segYearEnd-graphYearStart)*graphYearAngle;
     var shadeStart = graphYearAngleStart; // Starting position of the combined shadow = starts where the first segment starts
     var shadeEnd = graphYearAngleStart+(graphYearEnd-graphYearStart)*graphYearAngle-graphYearGap; // Ending position of the shadow = ends where the last segment ends
@@ -69,17 +69,31 @@ function arcSegment() {
     // First arc, in case it is fractionnal
     ctx.beginPath();
     ctx.strokeStyle = segment;
-    ctx.strokeStyle = 'rgba(255, 255, 255, 1)'; /* FIXME PPF */
+    ctx.strokeStyle = 'rgba(255, 255, 255, 1)'; /* FIXME PPF temporary colour */
     ctx.arc(segCenterX,segCenterY,radius,segStart,segFirstNewYear-graphYearGap,false);
     ctx.stroke();
-    // All other arcs
-    for (i=0; i<(segYearEnd-segYearStart-1); i++) {
+    // All other intermediary arcs
+    for (i=0; i<(segYearEnd-segYearStart-2); i++) {
         ctx.beginPath();
         ctx.strokeStyle = segment;
-        ctx.strokeStyle = 'rgba(255, 255, 255, 1)'; /* FIXME PPF */
-        ctx.arc(segCenterX,segCenterY,radius,segFirstNewYear+(i*graphYearAngle),segFirstNewYear+((i+1)*graphYearAngle-graphYearGap),false);
+        ctx.strokeStyle = 'rgba(255, 255, 255, 1)'; /* FIXME PPF temporary colour */
+        ctx.arc(
+            segCenterX,
+            segCenterY,
+            radius,
+            segFirstNewYear+(i*graphYearAngle),
+            segFirstNewYear+((i+1)*graphYearAngle-graphYearGap),
+            false
+        );
+        var segLastNewYear = segFirstNewYear+((i+1)*graphYearAngle);
         ctx.stroke();
     }
+    // Last arc, in case it is fractionnal
+    ctx.beginPath();
+    ctx.strokeStyle = segment;
+    ctx.strokeStyle = 'rgba(255, 255, 255, 1)'; /* FIXME PPF temporary colour */
+    ctx.arc(segCenterX,segCenterY,radius,segLastNewYear,(segYearEnd-Math.floor(segYearEnd))*graphYearAngle-graphYearGap,false);
+    ctx.stroke();
     //ctx.closePath();
 
 }
